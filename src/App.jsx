720b51fcfd05071a127019303311311570ca7fbc
Naluume <watyekeleawalin@gmail.com>
2025-03-27 11:18:44 +0300

    Started on project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="container bg-[url('/src/assets/images/background-desktop.png')] 
+    bg-cover bg-center w-lg min-h-screen m-auto"
+    >
+      <h1>
+        Hello Naluume Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Iure, omnis aut odio distinctio voluptates rem sapiente, suscipit
+        reprehenderit voluptate aspernatur iusto. Dicta ipsum, beatae voluptatem
+        similique ipsa cum iste odit.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
